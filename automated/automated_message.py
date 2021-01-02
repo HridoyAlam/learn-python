68de6344b27283fd1249e45daedbce3962cf8d64
HridoyAlam <optimus5289@gmail.com>
2021-01-02 21:54:24 +0600

    formted string

---
+++ automated/automated_message.py
@@ -0,0 +1,11 @@
+import time
+import pyautogui
+
+def SendMessage():
+    time.sleep(5)
+    text = open('message.txt')
+    for each_line in text:
+        pyautogui.typewrite(each_line)
+        pyautogui.press('enter')
+
+SendMessage()
---
+++ code with mosh/test.py
@@ -0,0 +1,8 @@
+'''
+name = input("What's your name? ")
+color = input("What's your favorite color? ")
+print(name + " likes "+color)
+'''
+year = input("What's your birth year? ")
+age = 2021 - int(year)
+print("You are "+ str(age)+ "'s old.")
